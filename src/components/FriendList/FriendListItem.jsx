be347b8aa7691679cc77a78f47249182d44fa650
Kamilucha <brnkamila2@gmail.com>
2023-07-01 13:32:56 +0300

    fix

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,7 +1,7 @@
 import { FriendItem, OnlineStatus, Avatar, Name } from "./FriendListItem.styled"
 
-export const FriendListItem = ({ avatar, name, isOnline, id }) => {
-    return <FriendItem key={id}>
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+    return <FriendItem>
         <OnlineStatus isOnline={isOnline}></OnlineStatus>
     <Avatar src={avatar} alt="User avatar" width="48" />
         <Name>{ name }</Name>
--- src/components/TransactionHistory/TransactionElement.jsx
+++ src/components/TransactionHistory/TransactionElement.jsx
@@ -1,9 +1,9 @@
 import { TableElement, TextTable} from "./TransactionElement.styled"
 
 
-export const TransactionElement = ({ id, type, amount, currency }) => {
+export const TransactionElement = ({ type, amount, currency }) => {
     return <tbody>
-    <TableElement key={id}>
+    <TableElement>
       <TextTable>{type}</TextTable>
       <TextTable>{amount}</TextTable>
       <TextTable>{currency}</TextTable>
